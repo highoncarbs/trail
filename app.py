667370ef3d6edd62d0c2ce607e863251d2dd70ea
padamsethia <padamsethia5@gmail.com>
2019-06-15 12:48:04 +0530

    Basic Master update

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, g, redirect, jsonify, url_for, request, session
 from flask_sqlalchemy import SQLAlchemy
 
@@ -10,29 +11,32 @@ app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
 
-from model import LoginForm, Users, SignupForm
+from model import Users , LoginForm , SignupForm , CustomerCategory , CustomerCategoryForm , City , State , Country ,\
+    CityForm , StateForm , CountryForm , Firms , FirmForm
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-logging.basicConfig(
-    filename='trail.log',
-    level=logging.DEBUG,
-    format='[TRAIL] %(levelname)-7.7s %(message)s'
-)
+# logging.basicConfig(
+#     filename='trail.log',
+#     level=logging.DEBUG,
+#     format='[TRAIL] %(levelname)-7.7s %(message)s'
+# )
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 
     form = LoginForm()
-    session['mssg'] = ""
     if form.validate_on_submit():
         user = Users.query.filter_by(username=form.username.data).first()
         if user:
             if check_password_hash(user.password, form.password.data):
                 login_user(user)
+                session['mssg'] = "Hey ! " + \
+                    str(current_user.username) + " . Welcome."
+
                 return redirect(url_for('home'))
             else:
                 session['mssg'] = "Invalid Username or Password"
@@ -46,7 +50,6 @@ def login():
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = SignupForm()
-    session['mssg'] = ""
     if form.validate_on_submit():
         user = Users.query.filter_by(email=form.email.data).first()
         if user is None:
@@ -59,6 +62,8 @@ def signup():
             db.session.add(new_user)
             db.session.commit()
             db.session.close()
+            session['mssg'] = "You're all set. Please Login. "
+
             return redirect(url_for('login'))
         else:
             session['mssg'] = "Email ID already in use. Please login"
@@ -70,27 +75,364 @@ def signup():
 
 @app.route('/forgot', methods=['GET', 'POST'])
 def forgot():
-    return render_template('login.html', subtitle="Forgot"), 200
+    return render_template('login.html', subtitle="Forgot", mssg=session['mssg']), 200
 
 
 @app.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    return render_template('base.html' , subtitle="Home") , 200
+    firm_form = FirmForm()
+    firm_list = db.session.query(Firms).all()
+    if firm_form.validate_on_submit():
+        firm_name = str(firm_form.firm_name.data).title()
+        check_one = db.session.query(Firms).filter_by(
+            firm_name=firm_name).first()
+        if check_one is None:
+            new_firm = Firms(firm_name=firm_form.firm_name.data, firm_city=firm_form.firm_city.data, firm_address_1=firm_form.firm_address_1.data,
+                             firm_state=firm_form.firm_state.data, firm_country=firm_form.firm_country.data, firm_pincode=firm_form.firm_pincode.data)
+            db.session.add(new_firm)
+            db.session.commit()
+            session['mssg'] = "Firm '" + \
+                str(firm_form.firm_name.data) + "' has been created."
+
+            return redirect('/home')
+        else:
+            session['mssg'] = "Firm '" + \
+                str(firm_form.firm_name.data) + "' already exists."
+            return redirect('/home')
+    return render_template('dash.html', subtitle="Home", mssg=session['mssg'], firm_form=firm_form, firm_list=firm_list), 200
+
+
+@app.route('/basic_master', methods=['GET', 'POST'])
+@login_required
+def basic_master():
+
+    city_list = db.session.query(
+        City).all()
+    city_form = CityForm()
+
+    if city_form.validate_on_submit():
+        if city_form.city_submit.data:
+
+            city_name = str(city_form.city.data).title()
+            check_one = City.query.filter_by(city=city_form.city.data).first()
+            if check_one is not None:
+                mssg = "City already exists "
+                return redirect(url_for('basic_master'))
+
+            else:
+                print(city_form.state.data)
+                state = city_form.state.data.state
+                country = city_form.country.data.country
+                new_city = City(city=city_name, state=state,
+                                country=country)
+
+                try:
+                    db.session.add(new_city)
+                    db.session.commit()
+                    db.session.close()
+                    session['mssg'] = "City - " + \
+                        city_name+".  Successfully added"
+
+                    return redirect(url_for('basic_master'))
+
+                except Exception as e:
+                    session['mssg'] = "Something went wrong"
+                    return redirect(url_for('basic_master'))
+
+        #     city_name = str(city_form.city.data).title()
+        #     check_one = db.session.query(
+        #         City).filter_by(city=city_name).first()
+        #     if check_one is None:
+        #         new_city = City(city=city_name)
+        #         db.session.add(new_city)
+        #         db.session.commit()
+        #         return redirect('/basic_master')
+
+        # elif city_form.city_update.data:
+        #     city_name = str(city_form.city.data).title()
+        #     check_one = db.session.query(
+        #         City).filter_by(city=city_name).first()
+        #     if check_one is not None:
+        #         check_one.city = city_name
+        #         db.session.add(new_city)
+        #         db.session.commit()
+        #         return redirect('/basic_master')
+
+    # State
+
+    state_list = db.session.query(
+        State).all()
+    state_form = StateForm()
+
+    if state_form.validate_on_submit():
+        if state_form.state_submit.data:
+            state_name = str(state_form.state.data).title()
+            check_one = db.session.query(
+                State).filter_by(state=state_name).first()
+            if check_one is None:
+                new_state = State(state=state_name)
+                db.session.add(new_state)
+                db.session.commit()
+                return redirect('/basic_master')
+
+    else:
+        print(state_form.errors)
+    # Country
+
+    country_list = db.session.query(
+        Country).all()
+    country_form = CountryForm()
+    if country_form.validate_on_submit():
+        if country_form.country_submit.data:
+            country_name = str(country_form.country.data).title()
+            check_one = db.session.query(
+                Country).filter_by(country=country_name).first()
+            if check_one is None:
+                new_country = Country(country=country_name)
+                db.session.add(new_country)
+                db.session.commit()
+                return redirect('/basic_master')
+
+        elif country_form.country_update.data:
+            country_name = str(country_form.country.data).title()
+            check_one = db.session.query(
+                Country).filter_by(country=country_name).first()
+            if check_one is None:
+                new_ccountry = Country(country=country_name)
+                db.session.add(new_country)
+                db.session.commit()
+                return redirect('/basic_master')
+
+    cust_list = db.session.query(
+        CustomerCategory).all()
+    cust_form = CustomerCategoryForm()
+
+    if cust_form.validate_on_submit():
+        if cust_form.health_submit.data:
+            cust_name = str(cust_form.health.data).title()
+            check_one = db.session.query(
+                CustomerCategory).filter_by(health=cust_name).first()
+            if check_one is None:
+                new_cust = CustomerCategory(health=cust_name)
+                db.session.add(new_cust)
+                db.session.commit()
+                return redirect('/basic_master')
+
+    else:
+        print(state_form.errors)
+    # else:
+    #     session['mssg'] = "Something's not right. Please check the entered data."
+    #     print(city_form.errors)
+
+    return render_template('basic_master.html', subtitle="Basic Master", mssg=session['mssg'], city_form=city_form, city_list=city_list,
+                           state_form=state_form, state_list=state_list, country_form=country_form, country_list=country_list,
+                           cust_list=cust_list,  cust_form= cust_form), 200
+
+
+# State Location Edits
+
+@app.route("/basic_master/state/update/<id>", methods=['POST'])
+@login_required
+def state_edit(id):
+    '''
+        Edits data from the Data Display Table
+        Requires Args :
+        INPUT : item_id      
+    '''
+    state_form = StateForm()
+    if state_form.state_update.data:
+        state_name = str(state_form.state.data).title()
+        check_one = db.session.query(
+            State).filter_by(id=id).first()
+        if check_one is not None:
+            check_one.state = state_name
+            db.session.commit()
+            session['mssg'] = "State updated successfully"
+            return redirect('/basic_master')
+        else:
+            session['mssg'] = "Something went wrong."
+            return redirect('/basic_master')
+
+
+@app.route("/basic_master/state/delete/<id>", methods=['POST', 'GET'])
+@login_required
+def state_delete(id):
+
+    check_one = db.session.query(
+        State).filter_by(id=id)
+    if check_one.first() is not None:
+        check_one.delete()
+        db.session.commit()
+        session['mssg'] = "State deleted successfully"
+        return redirect('/basic_master')
+    else:
+        session['mssg'] = "Something went wrong."
+        return redirect('/basic_master')
+
+# Country Location Edits
+
+
+@app.route("/basic_master/country/update/<id>", methods=['POST'])
+@login_required
+def country_edit(id):
+    '''
+        Edits data from the Data Display Table
+        Requires Args :
+        INPUT : item_id      
+    '''
+    country_form = CountryForm()
+    if country_form.country_update.data:
+        country_name = str(country_form.country.data).title()
+        check_one = db.session.query(
+            Country).filter_by(id=id).first()
+        if check_one is not None:
+            check_one.country = country_name
+            db.session.commit()
+            session['mssg'] = "Country updated successfully"
+            return redirect('/basic_master')
+        else:
+            session['mssg'] = "Something went wrong."
+            return redirect('/basic_master')
+
+
+@app.route("/basic_master/country/delete/<id>", methods=['POST', 'GET'])
+@login_required
+def country_delete(id):
+
+    check_one = db.session.query(
+        Country).filter_by(id=id)
+    if check_one.first() is not None:
+        check_one.delete()
+        db.session.commit()
+        session['mssg'] = "Country deleted successfully"
+        return redirect('/basic_master')
+    else:
+        session['mssg'] = "Something went wrong."
+        return redirect('/basic_master')
+
+
+# City Location Edit
+
+
+@app.route("/basic_master/city/update/<id>", methods=['POST'])
+@login_required
+def city_edit(id):
+    '''
+        Edits data from the Data Display Table
+        Requires Args :
+        INPUT : item_id      
+    '''
+    city_form = CityForm()
+    if city_form.city_update.data:
+        city_name = str(city_form.city.data).title()
+        check_one = db.session.query(
+            City).filter_by(id=id).first()
+        if check_one is not None:
+            check_one.city = city_name
+            check_one.state = city_form.state.data.state
+            check_one.country = city_form.country.data.country
+            db.session.commit()
+            session['mssg'] = "City updated successfully"
+            return redirect('/basic_master')
+        else:
+            session['mssg'] = "Something went wrong."
+            return redirect('/basic_master')
+
+
+@app.route("/basic_master/city/delete/<id>", methods=['POST', 'GET'])
+@login_required
+def city_delete(id):
+
+    check_one = db.session.query(
+        City).filter_by(id=id)
+    if check_one.first() is not None:
+        check_one.delete()
+        db.session.commit()
+        session['mssg'] = "City deleted successfully"
+        return redirect('/basic_master')
+    else:
+        session['mssg'] = "Something went wrong."
+        return redirect('/basic_master')
+
+# State Location Edits
+
+@app.route("/basic_master/health/update/<id>", methods=['POST'])
+@login_required
+def cust_edit(id):
+    '''
+        Edits data from the Data Display Table
+        Requires Args :
+        INPUT : item_id      
+    '''
+    cust_form = CustomerCategoryForm()
+    if cust_form.health_update.data:
+        cust_name = str(cust_form.health.data).title()
+        check_one = db.session.query(
+            CustomerCategory).filter_by(id=id).first()
+        if check_one is not None:
+            check_one.health = cust_name
+            db.session.commit()
+            session['mssg'] = "Customer Category updated successfully"
+            return redirect('/basic_master')
+        else:
+            session['mssg'] = "Something went wrong."
+            return redirect('/basic_master')
+
+
+@app.route("/basic_master/health/delete/<id>", methods=['POST', 'GET'])
+@login_required
+def cust_delete(id):
+
+    check_one = db.session.query(
+        CustomerCategory).filter_by(id=id)
+    if check_one.first() is not None:
+        check_one.delete()
+        db.session.commit()
+        session['mssg'] = "Customer Category deleted successfully"
+        return redirect('/basic_master')
+    else:
+        session['mssg'] = "Something went wrong."
+        return redirect('/basic_master')
+
+
+
+
+# Logout
 
 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
+    session['mssg'] = "You've been logged out."
+
     return redirect(url_for('login'))
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return Users.query.get(int(user_id))
 
-@app.route('/message/session' , methods=['POST'])
+
+@app.route('/message/session', methods=['POST'])
 @login_required
 def mssg_del():
-    session['mssg'] = ""
-    return jsonify({'mssg' :'Emptying session mssg' })
+    session['mssg'] = None
+    return jsonify({'mssg': 'Emptying session mssg'})
+
+
+@app.route('/firm/delete/<firm>', methods=["POST", "GET"])
+@login_required
+def firm_delete(firm):
+    try:
+        firm_get = db.session.query(Firms).filter_by(id=int(firm)).first()
+        firm_name = firm_get.firm_name
+        db.session.query(Firms).filter_by(id=int(firm)).delete()
+        db.session.commit()
+        session["mssg"] = "Firm - "+str(firm_name)+" is deleted."
+        return redirect('/')
+    except Exception as e:
+        logging.error('Error deleting Firm : ' + str(e))
+        session["mssg"] = "Couldn't delete firm."
+        return redirect('/')
--- model.py
+++ model.py
@@ -3,6 +3,8 @@ from wtforms.validators import InputRequired, Optional
 from flask_wtf import FlaskForm
 from datetime import datetime
 from flask_login import UserMixin
+from wtforms_alchemy.fields import QuerySelectField ,SelectMultipleField ,SelectField
+from wtforms.validators import InputRequired, Email, Length , DataRequired ,Regexp , Optional
 
 from app import db
 
@@ -22,3 +24,80 @@ class Users(db.Model, UserMixin):
     username = db.Column(db.String(50))
     email = db.Column(db.String(50))
     password = db.Column(db.String(250))
+
+def state_choice():
+    return db.session.query(State)
+
+def country_choice():
+    return db.session.query(Country)
+
+def city_choice():
+    return db.session.query(City)
+    
+class Firms(db.Model): 
+    id = db.Column(db.Integer,  primary_key=True)
+    firm_name = db.Column(db.String(50))
+    firm_city = db.Column(db.String(50))
+    firm_address_1 = db.Column(db.String(250))
+    firm_pincode = db.Column(db.Integer())
+    firm_country = db.Column(db.String(50))
+    firm_state = db.Column(db.String(50))
+
+class FirmForm(FlaskForm):
+    firm_name = StringField('firm_name')
+    firm_city = StringField('firm_city')
+    firm_address_1 = StringField('firm_add_1')
+    firm_pincode = StringField('firm_pincode')
+    firm_country = StringField('firm_country')
+    firm_state = StringField('firm_state')
+    firm_submit = SubmitField('firm_submit')
+
+#  Customer Category & Form
+
+class CustomerCategory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    health = db.Column(db.String(5), unique=True, nullable=False)
+
+class CustomerCategoryForm(FlaskForm):
+    health = StringField('health', validators=[InputRequired()])
+    health_submit = SubmitField('health_submit')
+    health_update = SubmitField('health_update')
+
+
+# State Model & Form
+
+class State(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    state = db.Column(db.String(30), unique=True, nullable=False)
+    
+class StateForm(FlaskForm):
+    state = StringField('state', validators=[InputRequired()])
+    state_submit = SubmitField('state_submit')
+    state_update = SubmitField('state_update')    
+
+# Country Model & Form
+    
+class Country(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    country = db.Column(db.String(30), unique=True, nullable=False)
+
+class CountryForm(FlaskForm):
+    country = StringField('country', validators=[InputRequired()])
+    country_submit = SubmitField('country_submit')
+    country_update = SubmitField('country_update')    
+
+# City Model & Form
+
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String(30), unique=True, nullable=False)
+    state = db.Column(db.String(30), nullable=False)
+    country = db.Column(db.String(30), nullable=False)
+
+class CityForm(FlaskForm):
+    city = StringField('city', validators=[InputRequired()])
+    state = QuerySelectField('state',validators=[InputRequired()] , query_factory=state_choice , allow_blank= False  , get_label='state')
+    country = QuerySelectField('country', validators=[InputRequired()], query_factory=country_choice , allow_blank= False ,get_label='country')
+    city_submit = SubmitField('city_submit')
+    city_update = SubmitField('city_update')    
+
